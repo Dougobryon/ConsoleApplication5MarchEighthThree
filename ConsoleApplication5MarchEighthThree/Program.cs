d4573537e6c121d9b10014c8153030eb208125eb
Your Name <you.example.com>
2016-05-16 13:55:30 -0400

    submitting EighthThree

---
+++ ConsoleApplication5MarchEighthThree/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5MarchEighthThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Movie theater for R-rated movie*/
+
+            Console.WriteLine("What is your age?:");
+            int age = int.Parse(Console.ReadLine());
+            if (age >= 17)
+            {
+                Console.WriteLine("Come on in");
+            }
+            else
+            {
+                Console.WriteLine("Go Home");
+            }
+                     
+            
+            }
+    }
+}
+
+
+
+
+    
